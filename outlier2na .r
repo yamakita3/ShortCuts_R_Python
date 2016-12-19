4ec1a3a8d642d76252a5d916e5fd627a5d470173
yamakita <yamakita3@gmail.com>
2016-12-20 05:02:45 +0900

    Update and rename outliner2na .r to outlier2na .r

--- outlier2na .r
+++ outlier2na .r
@@ -1,8 +1,8 @@
 ####
-#Outliner2na
+#outlierをnaに変換する関数。
 #
-outliner2na <- function(dat1,band1=2,quant=c(0.05,0.95)){
- outliner2navec <-function(dat=dat1,band=band1,quant=c(0.05,0.95)){
+outlier2na <- function(dat1,band1=2,quant=c(0.05,0.95)){
+ outlier2navec <-function(dat=dat1,band=band1,quant=c(0.05,0.95)){
  	for(i in (1+band):(length(dat)-band)){
  	 qu <- quantile(dat[(i-band):(i+band)],quant,na.rm=T)
  	 #naの入ったバッファをつけても良いかも。
@@ -17,9 +17,9 @@ outliner2na <- function(dat1,band1=2,quant=c(0.05,0.95)){
  	return(dat)
  }
  if(is.null(dim(x))==T){
- 	outliner2navec(dat=dat1,band=band1,quant)
+ 	outlier2navec(dat=dat1,band=band1,quant)
  }else{
- 	apply(dat1,2,function(x) outliner2navec(dat=x,band=band1))
+ 	apply(dat1,2,function(x) outlier2navec(dat=x,band=band1))
  }
  return(x)
 }
@@ -32,7 +32,7 @@ x <- matrix(c(1:3,0,5:6,NA,0,NA,10,11,100,13:18), ncol = 2);x
 #0をnaにする。
 x[x==0] <- NA;x
 #外れ値をnaにする
-x <- outliner2na(x,band1=2);x
+x <- outlier2na(x,band1=2);x
 #naを補完する。
 na.approx(x);x
 
@@ -42,6 +42,6 @@ na.approx(x);x
 #for(i in (1+d):(nrow(a)-d)){
 # qu <- quantile(a[(i-d):(i+d)],na.rm=T)
 # if(a[i]<qu[2]|a[i]>qu[2]){a[i] <- NA
-#}
-#apply(x,2,function(x) outliner2navec(dat=x,band=2))
+#}#
+#apply(x,2,function(x) outlier2navec(dat=x,band=2))
 
